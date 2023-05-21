63e1a73f703d5ac25f29c4a3478aa19d6a400f96
nickshakh <shakhbazyann@gmail.com>
2023-05-21 22:24:16 +0400

    My Skills

---
+++ MySkills/scripts/script.js
@@ -0,0 +1,9 @@
+function Nick() {
+    var x = document.getElementById("js");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "flex";
+      }
+    
+}
